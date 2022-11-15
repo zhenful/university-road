9b82aa93d6150264f6ff2b6b5ac5cbf1b6c7db95
yang <2110440958@qq.com>
2022-11-15 22:00:51 +0800

    代码

---
+++ yu/nacy.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 21104 on 2022/11/15.
+//
+
+#include "nacy.h"
---
+++ yu/nacy.h
@@ -0,0 +1,14 @@
+//
+// Created by 21104 on 2022/11/15.
+//
+
+#ifndef UNIVERSITY_ROAD_NACY_H
+#define UNIVERSITY_ROAD_NACY_H
+
+
+class nacy {
+
+};
+
+
+#endif //UNIVERSITY_ROAD_NACY_H
